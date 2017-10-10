fdc6fd7ad087a7a14347895f98572cd2285069ad
Juancesar <juancesarandrianto@gmail.com>
2017-10-10 21:40:38 +0700

    initial commit

--- src/providers/aprovalregister/aprovalregister.ts
+++ src/providers/aprovalregister/aprovalregister.ts
@@ -19,12 +19,12 @@ export class AprovalregisterProvider {
   getdata() : Observable<userModel[]> {
     let header = new Headers({'Authorization':localStorage.getItem('token')});
     let options = new RequestOptions({headers:header});
-     return this.http.get("http://localhost:3030/users",options).map(res => res.json().data as userModel[]);
+     return this.http.get("http://198.50.174.117/users",options).map(res => res.json().data as userModel[]);
   }
   ubahstatus(data,id){
     let header = new Headers({'content-Type':'application/json','Authorization':localStorage.getItem('token')});
     let options = new RequestOptions({headers:header});
     let body = JSON.stringify(data)
-     return this.http.patch("http://localhost:3030/users/"+id,body,options).map(res => res.json().data as userModel[]);
+     return this.http.patch("http://198.50.174.117/users/"+id,body,options).map(res => res.json().data as userModel[]);
   }
 }
