ede3f82790540dcfd386602ce5384db94e8c7b55
Juancesar <juancesarandrianto@gmail.com>
2017-11-30 21:47:39 +0700

    save aja dlu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,82 +38,7 @@ export class MyApp implements OnInit {
    
 }
 ngOnInit(){
-  this.datauser = JSON.parse(localStorage.getItem('datauser'))        
-  if(this.datauser.useracces == 'admin'){
-  this.pages = [
-    { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
-    { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },      
-    //{ title: 'Show Anim', component: SplashScreen,icon: 'ios-book-outline' },
-    { title: 'Data Siswa', component: DatasiswaPage,icon: 'logo-buffer' },
-    { title: 'Data Pegawai', component: DatapegawaiPage,icon: 'logo-buffer' },
-    { title: 'Approval Register', component: AprrovalregisterPage,icon: 'logo-buffer'},   
-    { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
-    { title: 'Import Data Pegawai', component: ImportdatapegawaiPage,icon: 'logo-buffer'},
-    { title: 'Import Data Siswa', component: ImportdatasiswaPage,icon: 'logo-buffer'},
-    // { title: 'Data Entry Attd', component:DataentryattdPage,icon: 'logo-buffer'},
-    { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
-    { title: 'Kelas', component: KelasPage,icon: 'logo-buffer'},
-    { title: 'User Access', component: UseraccessPage,icon: 'ios-key-outline' },
-    { title: 'Setting Jam Absen', component: SettingjamPage,icon: 'ios-settings-outline' },
-    { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },        
-    { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
-  ];
-}else if(this.datauser.useracces == 'siswa'){
-  this.pages = [
-    { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
-     { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },
-    { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
-    //{ title: 'Show Anim', component: SplashscreenPage,icon: 'ios-book-outline' },
-    { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
-    { title: 'Absensi Finger', component: AbsensisiswaPage,icon: 'ios-checkmark' },         
-    { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },      
-    { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
-  ];
-}else if(this.datauser.useracces == 'pegawai'){
-  this.pages = [
-    { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
-    { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
-    { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },
-    { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
-    { title: 'Absensi Finger', component: AbsensisiswaPage,icon: 'ios-checkmark' },         
-    { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },      
-    { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
-  ];
-}else if(this.datauser.useracces == 'guru'){
-  this.pages = [
-    { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
-    { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
-    { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },
-    { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
-    { title: 'Absensi Finger', component: AbsensisiswaPage,icon: 'ios-checkmark' },         
-    { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },      
-    { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
-  ];
-}else if(this.datauser.useracces == 'owner'){
-  this.pages = [
-    { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
-    { title: 'Approval Register', component: AprrovalregisterPage,icon: 'logo-buffer'},
-    { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
-  ];
-}else{
-  this.pages = [
-    { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
-    { title: 'Data Siswa', component: DatasiswaPage,icon: 'logo-buffer' },
-    { title: 'Data Pegawai', component: DatapegawaiPage,icon: 'logo-buffer' },
-    { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },
-    { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
-    { title: 'Approval Register', component: AprrovalregisterPage,icon: 'logo-buffer'},
-    { title: 'Import Data Pegawai', component: ImportdatapegawaiPage,icon: 'logo-buffer'},
-    { title: 'Import Data Siswa', component: ImportdatasiswaPage,icon: 'logo-buffer'},
-    { title: 'Data Entry Attd', component:DataentryattdPage,icon: 'logo-buffer'},
-    { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
-    { title: 'Kelas', component: KelasPage,icon: 'logo-buffer'},
-    { title: 'User Access', component: UseraccessPage,icon: 'ios-key-outline' },
-    { title: 'Setting Jam Absen', component: SettingjamPage,icon: 'ios-settings-outline' },
-    { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },      
-    { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
-  ];
-}
+ 
 }
 ionViewDidLoad() {
  
@@ -123,12 +48,88 @@ ionViewDidLoad() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.userdataok = JSON.parse(localStorage.getItem('datauser'));
-      if (this.userdataok == null) {
+      this.datauser = JSON.parse(localStorage.getItem('datauser'))        
+      
+     // this.userdataok = JSON.parse(localStorage.getItem('datauser'));
+      if (this.datauser == null) {
        this.nav.setRoot(LoginPage)
         // this.user is authenticated!
       }else{
-        
+        if(this.datauser.useracces == 'admin'){
+        this.pages = [
+          { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
+          { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },      
+          //{ title: 'Show Anim', component: SplashScreen,icon: 'ios-book-outline' },
+          { title: 'Data Siswa', component: DatasiswaPage,icon: 'logo-buffer' },
+          { title: 'Data Pegawai', component: DatapegawaiPage,icon: 'logo-buffer' },
+          { title: 'Approval Register', component: AprrovalregisterPage,icon: 'logo-buffer'},   
+          { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
+          { title: 'Import Data Pegawai', component: ImportdatapegawaiPage,icon: 'logo-buffer'},
+          { title: 'Import Data Siswa', component: ImportdatasiswaPage,icon: 'logo-buffer'},
+          // { title: 'Data Entry Attd', component:DataentryattdPage,icon: 'logo-buffer'},
+          { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
+          { title: 'Kelas', component: KelasPage,icon: 'logo-buffer'},
+          { title: 'User Access', component: UseraccessPage,icon: 'ios-key-outline' },
+          { title: 'Setting Jam Absen', component: SettingjamPage,icon: 'ios-settings-outline' },
+          { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },        
+          { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
+        ];
+      }else if(this.datauser.useracces == 'siswa'){
+        this.pages = [
+          { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
+           { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },
+          { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
+          //{ title: 'Show Anim', component: SplashscreenPage,icon: 'ios-book-outline' },
+          { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
+          { title: 'Absensi Finger', component: AbsensisiswaPage,icon: 'ios-checkmark' },         
+          { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },      
+          { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
+        ];
+      }else if(this.datauser.useracces == 'pegawai'){
+        this.pages = [
+          { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
+          { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
+          { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },
+          { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
+          { title: 'Absensi Finger', component: AbsensisiswaPage,icon: 'ios-checkmark' },         
+          { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },      
+          { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
+        ];
+      }else if(this.datauser.useracces == 'guru'){
+        this.pages = [
+          { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
+          { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
+          { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },
+          { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
+          { title: 'Absensi Finger', component: AbsensisiswaPage,icon: 'ios-checkmark' },         
+          { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },      
+          { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
+        ];
+      }else if(this.datauser.useracces == 'owner'){
+        this.pages = [
+          { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
+          { title: 'Approval Register', component: AprrovalregisterPage,icon: 'logo-buffer'},
+          { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
+        ];
+      }else{
+        this.pages = [
+          { title: 'Home', component: HomePage,icon: 'ios-home-outline' },
+          { title: 'Data Siswa', component: DatasiswaPage,icon: 'logo-buffer' },
+          { title: 'Data Pegawai', component: DatapegawaiPage,icon: 'logo-buffer' },
+          { title: 'Surat Keterangan', component: SuratketeranganPage,icon: 'ios-book-outline' },
+          { title: 'Profil', component: ProfilPage,icon: 'ios-contact-outline'},
+          { title: 'Approval Register', component: AprrovalregisterPage,icon: 'logo-buffer'},
+          { title: 'Import Data Pegawai', component: ImportdatapegawaiPage,icon: 'logo-buffer'},
+          { title: 'Import Data Siswa', component: ImportdatasiswaPage,icon: 'logo-buffer'},
+          { title: 'Data Entry Attd', component:DataentryattdPage,icon: 'logo-buffer'},
+          { title: 'Chatting', component: ChattingPage,icon: 'ios-chatbubbles-outline'},
+          { title: 'Kelas', component: KelasPage,icon: 'logo-buffer'},
+          { title: 'User Access', component: UseraccessPage,icon: 'ios-key-outline' },
+          { title: 'Setting Jam Absen', component: SettingjamPage,icon: 'ios-settings-outline' },
+          { title: 'Laporan Absensi', component: LaporanabsensiPage,icon: 'ios-book-outline' },      
+          { title: 'LogOut', component: LogoutComponent ,icon: 'ios-power-outline'},
+        ];
+      }
         this.nav.setRoot(HomePage);
       }
       this.statusBar.styleDefault();
